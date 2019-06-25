c785c975b3709e1942ca77428cecf06c23d0d1bd
Sabrina Hung <e0324471@u.nus.edu>
2019-06-26 00:15:55 +0800

    added google maps to feature 3

--- app/src/main/java/com/example/gogreenlah/featureThreeActivity.java
+++ app/src/main/java/com/example/gogreenlah/featureThreeActivity.java
@@ -8,13 +8,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class featureThreeActivity extends AppCompatActivity {
+public class featureThreeActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feature_three);
+
+        if (googleServicesAvailable()) {
+            setContentView(R.layout.activity_feature_three);
+        } else {
+            Toast.makeText(this, "Google Play Services Unavailable", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
     }
 
     public boolean googleServicesAvailable() {
@@ -29,5 +44,10 @@ public class featureThreeActivity extends AppCompatActivity {
             Toast.makeText(this, "Cannot connect to play services", Toast.LENGTH_LONG).show();
         }
         return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
     }
 }
